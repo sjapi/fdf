d862f7c6d1b78e0b668b66516557870a1cb10156
Arseniy Zolotarev <azolotar@c1r3s6.42yerevan.am>
2025-04-09 21:31:58 +0400

    Now i can draw maps

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: azolotar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:08:37 by azolotar          #+#    #+#             */
-/*   Updated: 2025/04/09 19:09:25 by azolotar         ###   ########.fr       */
+/*   Updated: 2025/04/09 21:28:21 by azolotar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	img_put_pixel(t_img *img, int x, int y, int color)
 }
 
 /* DDA Algorithm */
-void	draw_line(t_img *img, t_point p1, t_point p2)
+void	draw_line(t_img *img, t_point p1, t_point p2, int color)
 {
 	int			i;
 	t_dda_line	line;
@@ -38,17 +38,17 @@ void	draw_line(t_img *img, t_point p1, t_point p2)
 	line.x = p1.x;
 	line.y = p1.y;
 	i = 0;
-	img_put_pixel(img, (int)(line.x + 0.5), (int)(line.y + 0.5), 0x00ff00);
+	img_put_pixel(img, (int)(line.x + 0.5), (int)(line.y + 0.5), color);
 	while (i < line.steps)
 	{
 		line.x += line.x_inc;
 		line.y += line.y_inc;
-		img_put_pixel(img, (int)(line.x + 0.5), (int)(line.y + 0.5), 0x00ff00);
+		img_put_pixel(img, (int)(line.x + 0.5), (int)(line.y + 0.5), color);
 		i++;
 	}
 }
 
-void	draw_matrix(t_img *img, t_landscape *l)
+void	draw_matrix1(t_img *img, t_landscape *l)
 {
 	int	unit_len;
 	int	origin_x;
@@ -72,5 +72,37 @@ void	draw_matrix(t_img *img, t_landscape *l)
 			origin_x += unit_len + 1;
 		}
 		origin_y += unit_len + 1;
+	}
+}
+
+void	draw_matrix(t_img *img, t_landscape *l)
+{
+	int	tile_width = 20;
+	int	z = 15;
+	t_point origin = (t_point){ 300, 300 };
+	for (int i = 0; i < l->y_len; i++)
+	{
+		for (int j = 0; j < l->x_len; j++)
+		{
+			printf("%d\n", l->mtrx[i][j]);
+			int x = origin.x + (j - i) * tile_width;
+			int	y = origin.y + (j + i) * (tile_width / 2) - l->mtrx[i][j] * z;
+			img_put_pixel(img, x, y, 0xffffff);
+			if (j < l->x_len - 1)
+			{
+				printf("x\n");
+				int next_x = origin.x + (j + 1 - i) * tile_width;
+				int next_y = origin.y + (j + 1 + i) * (tile_width / 2) - l->mtrx[i][j + 1] * z;
+				draw_line(img, (t_point){x, y}, (t_point){next_x, next_y}, 0xffffff);
+			}
+			if (i < l->y_len - 1)
+			{
+				printf("y\n");
+				int next_x = origin.x + (j - i - 1) * tile_width;
+				int next_y = origin.y + (j + i + 1) * (tile_width / 2) - l->mtrx[i + 1][j] * z;
+				draw_line(img, (t_point){x, y}, (t_point){next_x, next_y}, 0xffffff);
+			}
+			printf("f\n");
+		}
 	}
 }
